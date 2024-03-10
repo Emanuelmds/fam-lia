7d9565a3422e6d24256a2089e405a0c36f207aa0
user.name <emanuel.sinik@hotmail.com>
2024-03-10 16:02:23 -0300

    conteudo pagina principal

---
+++ home.js
@@ -0,0 +1,12 @@
+// {
+//     var btn = document.createElement('BUTTON');
+//     var lbl = document.createTextNode("CLICK ME");        
+//     btn.appendChild(lbl);   
+//     btn.onclick = function()
+//     {
+//        window.history.go(0);
+//     }
+//     document.body.appendChild(btn);    
+// }
+
+// createButton();
